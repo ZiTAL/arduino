064925ad036d6389352b4792982d450c37deae23
zital <zitalman@gmail.com>
2015-12-08 13:27:37 +0100

    RELAY

--- RELAY/RELAY.ino
+++ RELAY/RELAY.ino
@@ -1,14 +1,19 @@
- #define IN1 8
+/*
+Jumper in JD-VCC and VCC PINS or weld it
+*/
+
+#define IN1 8
  
- void setup()
- {
-   pinMode(IN1, OUTPUT);
-   digitalWrite(IN1, LOW);
- }
+void setup()
+{
+  pinMode(IN1, OUTPUT);
+  digitalWrite(IN1, LOW);
+}
  
- void loop(){
-   digitalWrite(IN1, HIGH);  //Green LED on, Yellow LED off
-   delay(500);
-   digitalWrite(IN1, LOW);   //Yellow LED on, Green LED off
-   delay(500);
+void loop()
+{
+  digitalWrite(IN1, HIGH);
+  delay(500);
+  digitalWrite(IN1, LOW);
+  delay(500);
  }
